2468a045857d9bf9ebbcc2c01603c940c344565e
Tyuiu.PanasenkoRA@outlook.com <panasenkoroma@mail.ru>
2023-10-16 18:31:19 +0500

    Task6

---
+++ Tyuiu.PanasenkoRA.Sprint2.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.PanasenkoRA.Sprint2.Task6.V0.Lib
+{
+    public class DataService : ISprint2Task6V14
+    {
+        public string FindDayName(int k, int d)
+        {
+            
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint2.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanasenkoRA.Sprint2.Task6.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint2.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
